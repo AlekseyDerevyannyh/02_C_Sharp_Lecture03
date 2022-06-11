aa0c1320e0758febee045cce711e23d5c7acc21e
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-11 22:27:07 +0300

    add My Example 2 Multiplication table with for

---
+++ My_02_MultiplicationTable/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа вывода таблицы умножения с использованием цикла for
+using System;
+using static System.Console;
+
+Clear();
+for (int i = 2; i < 10; i ++) {
+	for (int j = 2; j < 10; j ++) {
+		WriteLine($"{i} * {j} = {i * j}");
+	}
+}
