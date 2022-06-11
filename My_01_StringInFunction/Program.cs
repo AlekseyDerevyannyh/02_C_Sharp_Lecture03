5afb2d06dbfd1445c223e3f3d769037d074c0fb1
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-11 20:41:51 +0300

    Initial commit

---
+++ My_01_StringInFunction/Program.cs
@@ -0,0 +1,14 @@
+﻿// Передача строк в функцию
+using System;
+using static System.Console;
+
+void Method21(string msg, int count) {
+	int i = 0;
+	while (i < count) {
+		WriteLine(msg);
+		i ++;
+	}
+}
+
+Method21(msg: "Текст", count: 4);
+
