4d1606e7b37cd5319f09689e2d511d9a43ed2a48
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-11 21:01:56 +0300

    add My Example 1 String in function

--- My_01_StringInFunction/Program.cs
+++ My_01_StringInFunction/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using static System.Console;
 
+Clear();
 void Method21(string msg, int count) {
 	int i = 0;
 	while (i < count) {
@@ -11,4 +12,5 @@ void Method21(string msg, int count) {
 }
 
 Method21(msg: "Текст", count: 4);
-
+Method21(count: 4, msg: "новый текст");
+Method21("strange text", 4);
