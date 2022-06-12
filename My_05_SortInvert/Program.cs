739f0f4b2a3bb53a16a3e93deca68a07a271193a
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-12 18:04:01 +0300

    add My Example 5 Sort array from max to min

---
+++ My_05_SortInvert/Program.cs
@@ -0,0 +1,34 @@
+﻿// Программа сортировки одномерного массива от большего к меньшему
+using System;
+using static System.Console;
+
+Clear();
+int[] array = new int[10];
+
+void Random (int[] array) {
+	for (int i = 0; i < array.Length; i ++) {
+		array[i] = new Random().Next(1, 10);
+	}
+}
+void PrintArray (int[] array) {
+	for (int i = 0; i < array.Length; i ++) {
+		Write($"{array[i]} ");
+	}
+}
+void SortArray (int[] array) {
+	for (int i = 0; i < array.Length - 1; i ++) {
+		int j_max = i;
+		for (int j = i + 1; j < array.Length; j ++) {
+			if (array[j] > array[j_max])	j_max = j;
+		}
+		int tmp = array[i];
+		array[i] = array[j_max];
+		array[j_max] = tmp;
+	}
+}
+
+Random(array);
+PrintArray(array);
+WriteLine();
+SortArray(array);
+PrintArray(array);
